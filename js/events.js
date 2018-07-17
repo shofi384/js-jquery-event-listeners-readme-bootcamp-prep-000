4d39d362ac8a333601c0ed35450736220eb0c2aa
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-17 15:41:41 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,28 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $(document).ready( function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('form:first').on('keydown', function(key) {
+    if(key.which === 71) {
+      alert('You presssed G');
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
 
