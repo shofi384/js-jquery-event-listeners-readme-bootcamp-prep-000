50b4a7f34d4f90d4b11ce9f29d456dfae05cf097
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-19 19:31:57 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,8 +11,13 @@ function frameIt() {
 }
 
 function pressIt() {
+<<<<<<< HEAD
   $('form:first').on('keydown', function(key) {
     if(key.which === 71) {
+=======
+  $('form:first').on('keydown', function() {
+    if($('input:first').val() === 'G') {
+>>>>>>> 7c5e4f73d325c060025d13184a44c98a6c100997
       alert('You presssed G');
     }
   })
